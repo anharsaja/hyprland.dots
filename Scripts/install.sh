a1bd26d0e440dc4e0928a128f76528858a4dfc9b
anharsaja <anharmuk12@gmail.com>
2024-12-11 20:29:26 +0700

    feat: script.sh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #|---/ /+--------------------------+---/ /|#
 #|--/ /-| Main installation script |--/ /-|#
-#|-/ /--| Prasanth Rangan          |-/ /--|#
+#|-/ /--| anharsaja - customing    |-/ /--|#
 #|/ /---+--------------------------+/ /---|#
 
 cat << "EOF"
